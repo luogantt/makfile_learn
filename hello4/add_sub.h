5fb1f1bf0f8c103204b2b567a97a28a4bed08793
luoganttccqy <ledi@xinyoudi.com>
2020-12-17 12:49:44 +0800

    hello

---
+++ hello4/add_sub.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+    > File Name: add_sub.h
+    > Author:
+    > Mail:
+    > Created Time: Tue 25 Jul 2017 10:38:53 AM CST
+ ************************************************************************/
+
+#ifndef _ADD_SUB_H
+#define _ADD_SUB_H
+int add(int a, int b);
+int sub(int a, int b);
+#endif
