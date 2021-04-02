c90b193661b8e3105bb8ae7632a5e0b637763cce
luoganttccqy <ledi@xinyoudi.com>
2021-04-02 17:54:20 +0800

    自动化推导

---
+++ hello5/main.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: main.c
+    > Author:
+    > Mail:
+    > Created Time: Tue 25 Jul 2017 10:40:22 AM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include"add_sub.h"
+int main()
+{
+    printf(" 2 + 3 = %d\n", add(2, 3));
+    printf(" 2 - 3 = %d\n", sub(2, 3));
+    return 0;
+}
