e093e7b45e7074561643310a890b6ef8b293e8ac
luogantt <luoganttccqy@gmail.com>
2021-12-02 11:47:05 +0800

    gpu-programming

---
+++ CUDA_Freshman/0_hello_world/build/CMakeFiles/Hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hello.dir/Hello_generated_hello_world.cu.o"
+  "Hello.pdb"
+  "Hello"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
