6b03ef521179b3607c09158e558d53d6296f91e0
luogantt <290328566@qq.com>
2022-07-26 12:14:21 +0800

    cmake install

---
+++ CMake简明教程/c13_添加库文件/build/MathFun/CMakeFiles/MathFunctions.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ledi/图片/３/makfile_learn/CMake简明教程/c13_添加库文件/MathFun/mysqrt.cxx" "/home/ledi/图片/３/makfile_learn/CMake简明教程/c13_添加库文件/build/MathFun/CMakeFiles/MathFunctions.dir/mysqrt.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
