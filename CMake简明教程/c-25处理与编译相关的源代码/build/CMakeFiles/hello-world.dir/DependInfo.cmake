9778dcce4a4be0d48657fc7afe2e311349987493
luogantt <290328566@qq.com>
2022-07-28 16:37:15 +0800

    处理与编译器相关代码

---
+++ CMake简明教程/c-25处理与编译相关的源代码/build/CMakeFiles/hello-world.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ledi/图片/３/makfile_learn/CMake简明教程/c-25处理与编译相关的源代码/hello-world.cpp" "/home/ledi/图片/３/makfile_learn/CMake简明教程/c-25处理与编译相关的源代码/build/CMakeFiles/hello-world.dir/hello-world.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILER_NAME=\"GNU\""
+  "IS_GNU_CXX_COMPILER"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
