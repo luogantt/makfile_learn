10606e54e639de3fcd7fceb4d6cc9c5b5b99cfad
luogantt <290328566@qq.com>
2022-07-28 17:27:14 +0800

    架构

---
+++ CMake简明教程/c26-检测处理器体系结构/build/CMakeFiles/arch-dependent.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ledi/图片/３/makfile_learn/CMake简明教程/c26-检测处理器体系结构/arch-dependent.cpp" "/home/ledi/图片/３/makfile_learn/CMake简明教程/c26-检测处理器体系结构/build/CMakeFiles/arch-dependent.dir/arch-dependent.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ARCHITECTURE=x86_64"
+  "IS_64_BIT_ARCH"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
