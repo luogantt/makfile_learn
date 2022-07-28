7067af84ed2751156f61d07f84fc2bde910cd6e7
luogantt <290328566@qq.com>
2022-07-28 15:49:25 +0800

    为语言设定标准

---
+++ CMake简明教程/c21-为语言设定标准/build/CMakeFiles/animals.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ledi/图片/３/makfile_learn/CMake简明教程/c21-为语言设定标准/Animal.cpp" "/home/ledi/图片/３/makfile_learn/CMake简明教程/c21-为语言设定标准/build/CMakeFiles/animals.dir/Animal.cpp.o"
+  "/home/ledi/图片/３/makfile_learn/CMake简明教程/c21-为语言设定标准/Cat.cpp" "/home/ledi/图片/３/makfile_learn/CMake简明教程/c21-为语言设定标准/build/CMakeFiles/animals.dir/Cat.cpp.o"
+  "/home/ledi/图片/３/makfile_learn/CMake简明教程/c21-为语言设定标准/Dog.cpp" "/home/ledi/图片/３/makfile_learn/CMake简明教程/c21-为语言设定标准/build/CMakeFiles/animals.dir/Dog.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "animals_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMake简明教程/c21-设置编译器选项/cxx-example/build/CMakeFiles/geometry.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ledi/图片/３/cmake-cookbook/chapter-01/recipe-08/cxx-example/geometry_circle.cpp" "/home/ledi/图片/３/cmake-cookbook/chapter-01/recipe-08/cxx-example/build/CMakeFiles/geometry.dir/geometry_circle.cpp.o"
+  "/home/ledi/图片/３/cmake-cookbook/chapter-01/recipe-08/cxx-example/geometry_polygon.cpp" "/home/ledi/图片/３/cmake-cookbook/chapter-01/recipe-08/cxx-example/build/CMakeFiles/geometry.dir/geometry_polygon.cpp.o"
+  "/home/ledi/图片/３/cmake-cookbook/chapter-01/recipe-08/cxx-example/geometry_rhombus.cpp" "/home/ledi/图片/３/cmake-cookbook/chapter-01/recipe-08/cxx-example/build/CMakeFiles/geometry.dir/geometry_rhombus.cpp.o"
+  "/home/ledi/图片/３/cmake-cookbook/chapter-01/recipe-08/cxx-example/geometry_square.cpp" "/home/ledi/图片/３/cmake-cookbook/chapter-01/recipe-08/cxx-example/build/CMakeFiles/geometry.dir/geometry_square.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
