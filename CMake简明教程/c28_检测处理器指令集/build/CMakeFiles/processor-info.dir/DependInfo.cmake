d3edba2632195af7bb6a81f7af53691fa76af17a
luogantt <290328566@qq.com>
2022-07-29 11:20:30 +0800

    检测处理器指令集

---
+++ CMake简明教程/c28_检测处理器指令集/build/CMakeFiles/processor-info.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ledi/图片/３/makfile_learn/CMake简明教程/c28_检测处理器指令集/processor-info.cpp" "/home/ledi/图片/３/makfile_learn/CMake简明教程/c28_检测处理器指令集/build/CMakeFiles/processor-info.dir/processor-info.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMake简明教程/c28_检测处理器指令集/build/CMakeFiles/processor-info.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/processor-info.dir/processor-info.cpp.o"
+  "processor-info"
+  "processor-info.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/processor-info.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
