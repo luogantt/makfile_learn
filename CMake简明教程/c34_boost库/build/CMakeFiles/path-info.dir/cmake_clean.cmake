6e71fed53507f35433b286989664b3f204707094
luogantt <290328566@qq.com>
2022-08-01 13:41:57 +0800

    boost1

---
+++ CMake简明教程/c34_boost库/build/CMakeFiles/path-info.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/path-info.dir/path-info.cpp.o"
+  "path-info"
+  "path-info.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/path-info.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
