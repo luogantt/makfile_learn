c90762dd4465c7baa649911ead386e746fe78e55
luogantt <290328566@qq.com>
2022-08-01 12:46:16 +0800

    egin

---
+++ CMake简明教程/c33_检测Eigen库/build/CMakeFiles/linear-algebra.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linear-algebra.dir/linear-algebra.cpp.o"
+  "linear-algebra"
+  "linear-algebra.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linear-algebra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
